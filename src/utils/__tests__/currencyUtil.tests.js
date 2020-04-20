ba1cd7dbc936cfa370ac031e438837ac0b44ad3e
YuanHao Chiang <yuanhao.ark@gmail.com>
2020-04-20 20:13:56 +0800

    add currency util for parsing and tests

---
+++ src/utils/__tests__/currencyUtil.tests.js
@@ -0,0 +1,32 @@
+import { parseAmount } from "../currencyUtil";
+
+describe('parseAmount', () => {
+
+  it("parses: '100 twd' as { '100', 'twd' }", () => {
+    expect(parseAmount('100 twd')).toEqual({ amount: 100, currency: 'twd' })
+  });
+
+  it("parses: 'eur50.30' as { '50.3', 'euro' }", () => {
+    expect(parseAmount('euro50.30')).toEqual({ amount: 50.3, currency: 'eur' })
+  });
+
+  it("parses: '$100EUR' as { '100', 'usd' } (first currency matched)", () => {
+    expect(parseAmount('$100EUR')).toEqual({ amount: 100, currency: 'usd' })
+  });
+
+  it("parses: '13000円' as { '13000', 'jpy' } (first currency matched", () => {
+    expect(parseAmount('13000円')).toEqual({ amount: 13000, currency: 'jpy' })
+  });
+
+  // TO DO (or no to do...) parse full-width numbers (japanese/chinese):
+  // it("parses: '５０.３円' as { '50.3', 'jpy' }", () => {
+  //   expect(parseAmount('５０.３円')).toEqual({ amount: 50.3, currency: 'jpy' })
+  // });
+
+});
+
+/*
+console.log(parseAmount('100.50 twd'));
+console.log(parseAmount('100.50 円'));
+console.log(parseAmount('15000￥'));
+*/
---
+++ src/utils/currencyUtil.js
@@ -0,0 +1,54 @@
+const MAIN_CURRENCY = 'usd';
+
+const CURRENCIES = {
+  'usd' : "$",
+  'twd' : "NT",
+  'crc' : "₡"
+};
+
+const CURRENCY_ALIASES = {
+  'usd' : [ 'us', '$' ],
+  'twd' : [ 'nt', 'ntd' ],
+  'crc' : [ 'colones', 'colon', 'colón' ],
+  'eur' : [ 'euro', '€', 'euros'],
+  'jpy' : [ 'yen', '￥', '円'],
+};
+
+// Creates a quick look-up dictionary for currencies in the shape: { [alias]: currency }, IE:
+// {
+//   'us'  : 'usd',
+//   '$'   : 'usd',
+//   'nt'  : 'twd',
+//   'ntd' : 'twd',
+//   ...
+// }
+
+export const CURRENCY_LOOKUP = (function createAliases() {
+
+  const aliases = {};
+
+  Object.entries(CURRENCY_ALIASES).forEach(([currency, alias]) => {
+    aliases[currency] = currency;
+    alias.forEach(alias => aliases[alias] = currency);
+  });
+
+  return aliases;
+})();
+
+// Receives a string and parses the number amount and if a currency (or alias) is used.
+// TODO: for now, it's only parsing based on the US format, other valid formats
+// 12,50,00,000 rupee / 1 000 rub / 1.500,50 colones etc/etc/
+// -> try currency.js / dinero.js / walletjs
+
+export function parseAmount(value) {
+  
+  const currencyAlias = value.replace(/[\d,.]/g, ' ').split(/\s/g).find(searchString => {
+    const possibleCurrency = searchString.trim();
+    return (possibleCurrency.length > 0 && CURRENCY_LOOKUP[possibleCurrency]);
+  });
+
+  const amount = parseFloat(currencyAlias ? value.replace(currencyAlias, ' ') : value);
+  const currency = currencyAlias ? CURRENCY_LOOKUP[currencyAlias] : null;
+
+  return { amount, currency };
+}
